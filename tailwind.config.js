ad68b48f125199a5cd1fce47000dcd2c9a93c85e
Sam.H <samhui3016@gmail.com>
2023-05-11 01:14:54 +0900

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "index.html",
+    "./src/**/*.{html,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
